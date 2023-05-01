6ead7aa44da1565e23a8d3230b4378643caeb28b
dibek <dibekpoudyal@gmail.com>
2023-05-01 13:08:37 +0545

    fixing reset

--- dbCommands.sql
+++ dbCommands.sql
@@ -56,3 +56,4 @@ ALTER TABLE users ADD CONSTRAINT users_uc_email UNIQUE (email);
 CREATE DATABASE test_snippetbox CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER 'test_web'@'localhost' IDENTIFIED BY 'Password@123';
 GRANT CREATE, DROP,ALTER, INDEX,SELECT,INSERT,UPDATE,DELETE ON test_snippetbox.* TO 'test_web'@'localhost';
+
